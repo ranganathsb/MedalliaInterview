8635dacc5b929c223e5b86fbf402de22075c77ca
xieqilu <xieqilu1989@gmail.com>
2015-01-22 14:50:03 -0800

    Update InOrderSuccessor.cs

--- InOrderSuccessor.cs
+++ InOrderSuccessor.cs
@@ -27,6 +27,16 @@ private List<TreeNode> list = new List<TreeNode>();
 
 
 //do not use extra memory space
+Parent pointer is NOT needed in this algorithm. The Algorithm is divided into two cases on the basis of right subtree of the input node being empty or not.
+
+Input: node, root // node is the node whose Inorder successor is needed.
+output: succ // succ is Inorder successor of node.
+
+1) If right subtree of node is not NULL, then succ lies in right subtree. Do following.
+Go to right subtree and return the node with minimum key value in right subtree.
+2) If right sbtree of node is NULL, then start from root and us search like technique. Do following.
+Travel down the tree, if a node’s data is greater than root’s data then go right side, otherwise go to left side.
+
 
 truct node * inOrderSuccessor(struct node *root, struct node *n)
 {
